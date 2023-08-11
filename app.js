4629e9fde7286759fd08d027ed90d1b43681cc2f
travyozza <84102421+travyozza@users.noreply.github.com>
2023-08-11 20:12:48 +1200

    env variables

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 // routes
 const books = require('./routes/api/books');
 

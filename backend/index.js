7ca864537306efbf96f371dee47c612c6df3ac72
travyozza <84102421+travyozza@users.noreply.github.com>
2023-08-13 23:25:29 +1200

    updated displayed prompt

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req, res) => res.send('Server online!'));
 
 // use Routes
 app.use('/api/books', books);
